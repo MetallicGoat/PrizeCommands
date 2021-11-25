ac77b119082e48d63014afd3b559d421418c3676
MetallicGoat <the.great.metallic.goat@gmail.com>
2021-11-24 20:14:08 -0500

    Remove bed break broadcast - Now exists in tweaks addon

--- src/main/java/me/metallicgoat/PrizeCommands/Events/BedBreakPrize.java
+++ src/main/java/me/metallicgoat/PrizeCommands/Events/BedBreakPrize.java
@@ -2,7 +2,6 @@ package me.metallicgoat.PrizeCommands.Events;
 
 import de.marcely.bedwars.api.event.arena.ArenaBedBreakEvent;
 import me.metallicgoat.PrizeCommands.Main;
-import me.metallicgoat.PrizeCommands.SendBroadcast;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,7 +13,6 @@ public class BedBreakPrize implements Listener {
         Main plugin = Main.getInstance();
         Player p = e.getPlayer();
         if(p != null) {
-            SendBroadcast.broadcastBedBreak(e.getArena(), plugin.getConfig().getStringList("bed-break-prize.broadcast"), e);
             String name = p.getName();
             for (String command : plugin.getConfig().getStringList("bed-break-prize.commands")) {
                 if (command != null && !command.equals("")) {
